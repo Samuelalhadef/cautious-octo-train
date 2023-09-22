5bb6e617f216a2bcbeda8e345ed6aee9c6437f44
Samuelalhadef <145781410+Samuelalhadef@users.noreply.github.com>
2023-09-22 16:02:10 +0200

    Add files via upload

---
+++ condition/fonction.js
@@ -0,0 +1,12 @@
+let aQ="jean";
+let bBis="Paul";
+let result=checkName(aQ,bBis)
+
+function checkName(name1, name2){
+    if(name1===name2){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(result);
